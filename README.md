dbe3856a74606bb9507c55ecbba5baed0a3b8275
Hexagon <hexagon@56k.guru>
2024-04-23 20:43:48 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# @pup/api-definitions
+
+**Purpose:**
+
+This package provides core TypeScript type definitions and interfaces that
+describe the data structures and responses of the Pup REST API. It's a
+foundational dependency for the following packages within the Pup project:
+
+- `@pup/pup` - <https://github.com/hexagon/pup>
+- `@pup/api-client` - <https://github.com/hexagon/pup-api-client>
+
+**Usage**
+
+The `@pup/api-definitions` package is not intended for direct installation.
+Instead, the Pup development team and contributors primarily use it for the
+following:
+
+- **Type-safe API Interactions:** Ensure consistent and reliable communication
+  between Pup components by using the provided types in other Pup-related
+  packages.
+- **Documentation and Code Understanding:** The interfaces serve as a clear
+  reference for the Pup API's structure, aiding development and maintenance.
+
+**Development and Contributions**
+
+If you're actively developing components for the Pup project, this package's
+types will be essential.
+
+Please open a PR or issue if you have any ideas!
+
+This library follows semantic versioning. For a detailed history of changes to
+the API definitions, please refer to the [CHANGELOG.md](./CHANGELOG.md).
+
+**License**
+
+This package is released under the MIT License. See [LICENSE](./LICENSE) for
+details.
---
+++ types.ts
@@ -0,0 +1,120 @@
+export interface ApiPaths {
+  temporaryStorage?: string;
+  persistentStorage?: string;
+  configFilePath?: string;
+}
+
+export interface ApiProcessData {
+  status: ApiProcessInformation;
+  config: ApiProcessConfiguration;
+}
+
+export enum ApiProcessState {
+  CREATED = 0,
+  STARTING = 100,
+  RUNNING = 200,
+  STOPPING = 250,
+  FINISHED = 300,
+  ERRORED = 400,
+  EXHAUSTED = 450,
+  MIXED = 500, // Used for clusters with instances of varying modes
+}
+
+export interface ApiProcessInformation {
+  id: string;
+  status: ApiProcessState;
+  code?: number;
+  signal?: string;
+  pid?: number;
+  started?: Date;
+  exited?: Date;
+  blocked?: boolean;
+  restarts?: number;
+  updated: Date;
+  pendingRestartReason?: string;
+  type: "cluster" | "process" | "worker";
+}
+
+export interface ApiClusterConfiguration {
+  instances?: number;
+  commonPort?: number;
+  startPort?: number;
+  strategy?: string;
+}
+
+export interface ApiProcessConfiguration {
+  id: string;
+  cmd?: string;
+  worker?: string[];
+  env?: Record<string, string>;
+  cwd?: string;
+  cluster?: ApiClusterConfiguration;
+  pidFile?: string;
+  watch?: string[];
+  autostart?: boolean;
+  cron?: string;
+  timeout?: number;
+  overrun?: boolean;
+  logger?: unknown;
+  restart?: string;
+  restartDelayMs?: number;
+  restartLimit?: number;
+}
+
+export interface ApiApplicationState {
+  pid: number;
+  version: string;
+  status: string;
+  updated: string;
+  started: string;
+  memory: ApiMemoryUsage;
+  port: number;
+  systemMemory: ApiSystemMemory;
+  loadAvg: number[];
+  osUptime: number;
+  osRelease: string;
+  denoVersion: { deno: string; v8: string; typescript: string };
+  type: string;
+  processes: ApiProcessInformation[];
+}
+
+export interface ApiLogItem {
+  severity: string;
+  category: string;
+  text: string;
+  processId: string;
+  timeStamp: number;
+}
+export interface ApiMemoryUsage {
+  external?: number,
+  heapTotal: number,
+  heapUsed: number,
+  rss: number
+}
+/** Based on Deno.SystemMemoryInfo */
+export interface ApiSystemMemory {
+  /** Total installed memory in bytes. */
+  total: number;
+  /** Unused memory in bytes. */
+  free: number;
+  /** Estimation of how much memory, in bytes, is available for starting new
+   * applications, without swapping. Unlike the data provided by the cache or
+   * free fields, this field takes into account page cache and also that not
+   * all reclaimable memory will be reclaimed due to items being in use.
+   */
+  available: number;
+  /** Memory used by kernel buffers. */
+  buffers: number;
+  /** Memory used by the page cache and slabs. */
+  cached: number;
+  /** Total swap memory. */
+  swapTotal: number;
+  /** Unused swap memory. */
+  swapFree: number;
+}
+export interface ApiTelemetryData {
+  sender?: string;
+  memory: ApiMemoryUsage;
+  sent: string;
+  cwd: string;
+}
